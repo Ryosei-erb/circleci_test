97d51a7317b5dfa309445c5cb4faa035e0b168c3
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-04-07 09:18:13 +0900

    Add the rubocop and rspec check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:user) { User.create(name: "Name", email: "Email")}
+  let!(:user) { User.create(name: "Name", email: "Email") }
 
   it { expect(user).to be_valid }
-  it "Error on purpose" do
-    expect(product).to be_valid
-  end
+  # it "Error on purpose" do
+  #   expect(product).to be_valid
+  # end
 end
